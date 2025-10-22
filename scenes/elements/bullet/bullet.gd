3f7139cff6c4183319a8e7d100407704db2d3a33
G_aku <rafareyesmontilla@gmail.com>
2025-10-22 16:06:33 +0200

    Added bullets

--- scenes/elements/bullet/bullet.gd
+++ scenes/elements/bullet/bullet.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 class_name Bullet
 
 var dir: Vector2 = Vector2.ZERO
-var speed: float = 3500
+var speed: float = 5500
 
 func _process(delta: float) -> void:
 	global_position += dir * speed  * delta
